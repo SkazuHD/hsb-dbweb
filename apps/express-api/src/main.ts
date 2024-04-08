ed14ef61848ec156f0aa2683ff70e0ce53001179
SkazuHD <voda0110@gmail.com>
2024-04-08 15:57:02 +0200

    init: typesafety

--- apps/express-api/src/main.ts
+++ apps/express-api/src/main.ts
@@ -1,34 +1,17 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
+import { NextFunction, Request, Response } from "express";
+
 import * as path from 'path';
 import * as process from "process";
 import * as argon2 from "@node-rs/argon2";
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.use(express.json());
-
-app.use((req, res, next) => {
-  res.appendHeader('Access-Control-Allow-Origin', '*');
-  next();
-})
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-})
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to express-api!' });
+app.get('/api', (req: Request, res: Response) => {
+  res.send({ message: 'Express API Work!' });
 });
 
-app.post('/api/login', (req, res) => {
+app.post('/api/login', (req: Request, res: Response) => {
   const {username, password} = req.body;
   if (!username || !password) {
     res.status(400).send({message: 'Missing username or password'})
@@ -42,7 +25,7 @@ app.post('/api/login', (req, res) => {
   }
   res.status(200).send({message: 'Login successful'})
 })
-app.post('/api/register', async (req, res) => {
+app.post('/api/register', async (req: Request, res: Response) => {
   const {username, password} = req.body;
   if (!username || !password) {
     res.status(400).send({message: 'Missing username or password'})
@@ -53,8 +36,10 @@ app.post('/api/register', async (req, res) => {
 
   res.status(201)
 })
-
-app.get('/api/*', (req, res) => {
+app.get('/api/error', (req: Request, res:Response, next: NextFunction) => {
+  next(new Error('Test error'));
+});
+app.get('/api/*', (req: Request, res: Response) => {
   res.status(404).send({ message: 'Not Found' });
 });
 
@@ -62,4 +47,16 @@ const port = process.env.PORT;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
-server.on('error', console.error);
+
+app.use(express.json());
+
+app.use((req: Request, res:Response, next: NextFunction) => {
+  res.appendHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use((err : Error, req: Request, res:Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send({message: err.message ? err.message : 'Something broke!'});
+})
