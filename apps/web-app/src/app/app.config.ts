28bde756231386ccd995fc794e746736a0ada6b3
SkazuHD <voda0110@gmail.com>
2024-04-20 18:08:23 +0200

    feat: create AppShell Component  - Custom Theming + Darkmode

--- apps/web-app/src/app/app.config.ts
+++ apps/web-app/src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {HttpClientModule} from "@angular/common/http";
 import {provideNativeDateAdapter} from "@angular/material/core";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), importProvidersFrom(HttpClientModule), provideNativeDateAdapter()],
+  providers: [provideRouter(appRoutes), importProvidersFrom(HttpClientModule), provideNativeDateAdapter(), importProvidersFrom(BrowserAnimationsModule)],
 };
