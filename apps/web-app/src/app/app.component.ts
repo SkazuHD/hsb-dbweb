1e8780ea08f034587a8e8520f8c3ebd66f318e3a
SkazuHD <voda0110@gmail.com>
2024-04-23 19:46:54 +0200

    feat(web-app): Sample implementation of sse

--- apps/web-app/src/app/app.component.ts
+++ apps/web-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ApiServiceService } from './services/api-service.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { AppShellComponent } from './app-shell/app-shell';
 import { ArticleComponent } from './article/article.component';
 
@@ -14,4 +15,9 @@ import { ArticleComponent } from './article/article.component';
 export class AppComponent {
   apiService = inject(ApiServiceService);
   title = 'hsb-dbweb';
+  constructor() {
+    this.apiService.events$.pipe(takeUntilDestroyed()).subscribe((event) => {
+      console.log(JSON.parse(event.data));
+    });
+  }
 }
--- apps/web-app/src/app/services/api-service.service.ts
+++ apps/web-app/src/app/services/api-service.service.ts
@@ -1,14 +1,34 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, retry, RetryConfig } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiServiceService {
-
   constructor(private http: HttpClient) {
     http.get('http://127.0.0.1:4201/api').subscribe((data) => {
       console.log(data);
     });
+  }
+  private constructSSERequest(url: string) {
+    const retryConfig: RetryConfig = {
+      delay: 1000,
+      resetOnSuccess: true,
+    };
+    return new Observable<MessageEvent>((observer) => {
+      const eventSource = new EventSource(url);
+      eventSource.onmessage = (event) => observer.next(event);
+      eventSource.onerror = (error) => observer.error(error);
+    }).pipe(
+      retry(retryConfig),
+      map((event: MessageEvent) => {
+        return event;
+      }),
+    );
+  }
+
+  get events$(): Observable<MessageEvent> {
+    return this.constructSSERequest('http://localhost:4201/events');
   }
 }
