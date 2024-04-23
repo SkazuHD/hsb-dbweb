627851d52f35e803d3078a11193d8ead535b4e3d
SkazuHD <voda0110@gmail.com>
2024-04-23 19:08:16 +0200

    feat(express-api): Sample implementation of sse

--- apps/express-api/src/main.ts
+++ apps/express-api/src/main.ts
@@ -1,66 +1,13 @@
-import express from 'express';
-import { NextFunction, Request, Response } from "express";
+import express, { NextFunction, Request, Response } from 'express';
 
 import * as path from 'path';
-import * as process from "process";
+import * as process from 'process';
 //import * as argon2 from "@node-rs/argon2";
 import serverLess from 'serverless-http';
+import { sseConnections, sseMiddleware, SSEResponse } from './sse';
 
 const app = express();
 const router = express.Router();
-
-router.use(express.json());
-router.use((req: Request, res:Response, next: NextFunction) => {
-  res.appendHeader('Access-Control-Allow-Origin', '*');
-  next();
-})
-router.get('/', (req: Request, res: Response) => {
-  res.send({ message: 'Express API Work!' });
-});
-router.get('/hello', (req: Request, res: Response) => {
-  res.send({ message: 'Hello World!' });
-});
-
-router.post('/login', (req: Request, res: Response) => {
-  const {username, password} = req.body;
-  if (!username || !password) {
-    res.status(400).send({message: 'Missing username or password'})
-    return;
-  }
-  //TODO DB STUFF
-  const isValid =  true//argon2.verify(password, 'HASH_FROM_DB')
-  if (!isValid) {
-    res.status(403).send({message: 'Invalid password'})
-    return;
-  }
-  res.status(200).send({message: 'Login successful'})
-})
-router.post('/register', async (req: Request, res: Response) => {
-  const {username, password} = req.body;
-  if (!username || !password) {
-    res.status(400).send({message: 'Missing username or password'})
-    return;
-  }
-  //const hash = await argon2.hash(password)
-  //TODO DB STUFF
-
-  res.status(201)
-})
-router.get('/error', (req: Request, res:Response, next: NextFunction) => {
-  next(new Error('Test error'));
-});
-router.get('/*', (req: Request, res: Response) => {
-  res.status(404).send({ message: 'Not Found' });
-});
-
-router.use((err : Error, req: Request, res:Response, next: NextFunction) => {
-  console.error(err.stack);
-  res.status(500).send({message: err.message ? err.message : 'Something broke!'});
-})
-
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.use('/api/', router);
 
 const port = process.env.PORT;
 const server = app.listen(port, () => {
@@ -68,3 +15,75 @@ const server = app.listen(port, () => {
 });
 
 export const handler = serverLess(app);
+
+app
+  .use('/api/', router)
+  .use('/assets', express.static(path.join(__dirname, 'assets')));
+router
+  .use(express.json())
+  .use((req: Request, res: Response, next: NextFunction) => {
+    res.appendHeader('Access-Control-Allow-Origin', '*');
+    //res.flushHeaders();
+    next();
+  })
+  .use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res
+      .status(500)
+      .send({ message: err.message ? err.message : 'Something broke!' });
+  });
+//App routes
+
+app
+  .get('/events', sseMiddleware, (req: Request, res: SSEResponse) => {
+    res.sseConnection.send({ message: 'Welcome to the SSE connection' });
+  })
+  .get('/hello', (req: Request, res: Response) => {
+    sseConnections.forEach((sseRes) => {
+      sseRes.sseConnection.send({
+        message: 'Ping from /hello',
+        time: Date.now(),
+      });
+    });
+    res.send({ message: 'Hello World!' });
+  });
+
+//API routes
+router
+  .get('/', (req: Request, res: Response) => {
+    res.send({ message: 'Express API Work!' });
+  })
+  .get('/hello', (req: Request, res: Response) => {
+    res.send({ message: 'Hello World!' });
+  })
+  .post('/login', (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      res.status(400).send({ message: 'Missing username or password' });
+      return;
+    }
+    //TODO DB STUFF
+    const isValid = true; //argon2.verify(password, 'HASH_FROM_DB')
+    if (!isValid) {
+      res.status(403).send({ message: 'Invalid password' });
+      return;
+    }
+    res.status(200).send({ message: 'Login successful' });
+  })
+  .post('/register', async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      res.status(400).send({ message: 'Missing username or password' });
+      return;
+    }
+    //const hash = await argon2.hash(password)
+    //TODO DB STUFF
+
+    res.status(201);
+  })
+  .get('/error', (req: Request, res: Response, next: NextFunction) => {
+    next(new Error('Test error'));
+  })
+  .get('/*', (req: Request, res: Response) => {
+    res.status(404).send({ message: 'Not Found' });
+  });
