cf21e64db445edb425136f1591a1a5e68948e5b8
SkazuHD <voda0110@gmail.com>
2024-04-26 17:46:29 +0200

    feat: meta tag service to add meta tags

--- apps/web-app/src/app/article/article.component.ts
+++ apps/web-app/src/app/article/article.component.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Article } from '../types/types';
+import { MetatagService } from '../services/metatag.service';
 
 @Component({
   selector: 'app-article',
@@ -9,6 +10,16 @@ import { Article } from '../types/types';
   templateUrl: './article.component.html',
   styleUrl: './article.component.css',
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit {
+  private meta: MetatagService = inject(MetatagService);
   article = input.required<Article>();
+
+  ngOnInit(): void {
+    this.meta.addTagsForArticle(this.article());
+    this.meta.addTagsForArticle({
+      title: 'My Article',
+      content: 'This is my article',
+      id: '123',
+    });
+  }
 }
