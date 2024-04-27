8c06f0ea6998a3de7f779c94e8bac2fbf502d803
aqfel <131907659+aqfel@users.noreply.github.com>
2024-04-27 13:51:33 +0200

    1st-version-landing-page

--- apps/web-app/src/app/app.component.ts
+++ apps/web-app/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import {Component, inject} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {ApiService} from "./services/api.service";
-
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ApiServiceService } from './services/api-service.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AppShellComponent } from './app-shell/app-shell';
+import { InfoPageComponent } from './components/infopage/info-page.component';
 
 @Component({
   standalone: true,
@@ -27,5 +29,4 @@ export class AppComponent {
       document.body.classList.add('dark');
     }
   }
-
 }
