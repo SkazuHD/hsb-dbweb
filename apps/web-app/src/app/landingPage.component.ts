f839e433cda23f7b85e8f08f6d49d6afa889b206
aqfel <131907659+aqfel@users.noreply.github.com>
2024-04-27 17:23:03 +0200

    added newsfeed component

--- apps/web-app/src/app/landingPage.component.ts
+++ apps/web-app/src/app/landingPage.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { NewsfeedComponent } from './newsfeed.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButton, NewsfeedComponent],
   templateUrl: './landingPage.component.html',
   styleUrl: './landingPage.component.css',
 })
