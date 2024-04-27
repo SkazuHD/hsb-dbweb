9083f34ac413043b443e637a2f84953b3ecef946
SkazuHD <voda0110@gmail.com>
2024-04-27 10:33:30 +0200

    feat: add service worker for pwa

--- apps/web-app/src/app/app.config.ts
+++ apps/web-app/src/app/app.config.ts
@@ -1,11 +1,15 @@
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, isDevMode} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {HttpClientModule} from "@angular/common/http";
 import {provideNativeDateAdapter} from "@angular/material/core";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), importProvidersFrom(HttpClientModule), provideNativeDateAdapter(), importProvidersFrom(BrowserAnimationsModule)],
+  providers: [provideRouter(appRoutes), importProvidersFrom(HttpClientModule), provideNativeDateAdapter(), importProvidersFrom(BrowserAnimationsModule), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })],
 };
