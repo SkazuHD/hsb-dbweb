061a3f28a98a8fef69fd90e235e42b20ec0b4a44
RayLaxon <RayLaxon@gmx.de>
2024-05-04 14:45:44 +0200

    fix: jonah special

--- apps/web-app/src/app/services/api-service.service.ts
+++ apps/web-app/src/app/services/api-service.service.ts
@@ -1,7 +1,7 @@
-import {inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {map, Observable, of, retry, RetryConfig} from 'rxjs';
-import {InfoText} from "../utils/types/types";
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, of, retry, RetryConfig } from 'rxjs';
+import { Contact, InfoText } from '../utils/types/types';
 
 @Injectable({
   providedIn: 'root',
@@ -36,34 +36,55 @@ export class ApiServiceService {
     return this.constructSSERequest('http://localhost:4201/events');
   }
 
+  getContact(): Observable<Contact> {
+    return of({
+        street:'Beethovenstr. 18a',
+        location:'47226 Duisburg-Rheinhausen',
+        additionalDescription:'im Regenbogenhaus',
+        titleContact: 'Kontakt',
+        titleLocation: 'Trainingshalle',
+        titleMap: 'Karte',
+        content:'Wir freuen uns auf Fragen und Mitteilungen! Sendet einfach eine E-Mail oder ruft an.',
+        email: 'info@atemschulung-griepentrog.de',
+        name: 'Peter Griepentrog',
+        telephoneNumbers: 'tel:02065679631',
+        telephone: 'Tel.: 020 65 - 67 96 31',
+        faxNumbers: 'tel:0206563841',
+        fax: 'Fax: 020 65 - 6 38 41',
+        mobilNumbers: 'tel:01709042408',
+        mobil: 'Mobil: 0170 - 90 42 408',
+    })
+  }
+
   getInfo(id: string): Observable<InfoText> {
     return of({
       title: 'Some Example',
-      content: 'remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove ',
+      content:
+        'remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove remove ',
       schedule: [
         {
-
           time: '16:00 - 17:00 Uhr',
-          age: '4 - 6 Jahre'
+          age: '4 - 6 Jahre',
         },
         {
           time: '17:00 - 18:00 Uhr',
-          age: '7 - 8 Jahre'
+          age: '7 - 8 Jahre',
         },
         {
           time: '18:00 - 19:00 Uhr',
-          age: '9 - 11 Jahre'
+          age: '9 - 11 Jahre',
         },
         {
           time: '19:00 - 20:00 Uhr',
-          age: '12 - 14 Jahre'
+          age: '12 - 14 Jahre',
         },
         {
           time: '20:00 - 21:30 Uhr',
-          age: '15 - 80 Jahre'
-        }],
+          age: '15 - 80 Jahre',
+        },
+      ],
       schedule_title: 'Trainingszeiten',
-      schedule_days: 'Montag und Freitag'
+      schedule_days: 'Montag und Freitag',
     });
   }
 }
