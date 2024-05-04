3d907cbe87c4cd140eba259612e9cd4946a57824
RayLaxon <RayLaxon@gmx.de>
2024-05-04 14:19:26 +0200

    fix: new branch

--- apps/web-app/src/app/services/metatag.service.ts
+++ apps/web-app/src/app/services/metatag.service.ts
@@ -1,50 +1,60 @@
-import { inject, Injectable } from '@angular/core';
-import { Meta } from '@angular/platform-browser';
-import { Article } from '../utils/types/types';
+import { inject, Injectable } from "@angular/core";
+import { Meta } from "@angular/platform-browser";
+import { Article, Contact } from "../utils/types/types";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class MetatagService {
   private meta: Meta = inject(Meta);
 
+  addTagsForContact(contact: Contact): void {
+    const tags = [
+      {
+        name: "description",
+        content: this.generateContactDesciption(contact),
+      },
+    ];
+    tags.forEach((tag) => this.meta.updateTag(tag));
+  }
+
   addTagsForArticle(article: Article): void {
     const tags = [
       {
-        name: 'author',
-        content: article.author ?? 'Unknown Author',
+        name: "author",
+        content: article.author ?? "Unknown Author",
       },
       {
-        name: 'description',
+        name: "description",
         content: article.content ?? article.subtitle,
       },
       {
-        name: 'keywords',
+        name: "keywords",
         content: this.generateKeywords(article),
       },
       {
-        name: 'og:title',
+        name: "og:title",
         content: article.title,
       },
       {
-        name: 'og:type',
-        content: 'article',
+        name: "og:type",
+        content: "article",
       },
       {
-        name: 'og:description',
+        name: "og:description",
         content: article.content,
       },
       {
-        name: 'og:image',
-        content: article.image?.url ?? '',
+        name: "og:image",
+        content: article.image?.url ?? "",
       },
       {
-        name: 'og:url',
+        name: "og:url",
         content: window.location.href,
       },
       {
-        name: 'twitter:card',
-        content: 'summary_large_image',
+        name: "twitter:card",
+        content: "summary_large_image",
       },
     ];
 
@@ -56,6 +66,23 @@ export class MetatagService {
       article?.author,
       article?.date,
       article?.subtitle,
-    ].join(', ');
+    ].join(", ");
+  }
+
+  private generateContactDesciption(contact: Contact): string {
+    return [
+      contact?.street,
+      contact?.location,
+      contact?.additionalDescription,
+      contact?.titleContact,
+      contact?.titleLocation,
+      contact?.titleMap,
+      contact?.content,
+      contact?.email,
+      contact?.name,
+      contact?.telephone,
+      contact?.fax,
+      contact?.mobil,
+    ].join(" ");
   }
 }
--- apps/web-app/src/app/utils/types/types.ts
+++ apps/web-app/src/app/utils/types/types.ts
@@ -11,19 +11,27 @@ export type Article = {
   id: string;
   content: string;
 };
+
+export type Contact = {
+  additionalDescription: string;
+  location: string;
+  street: string;
+  titleMap: string;
+  titleLocation: string;
+  titleContact: string;
+  content: string;
+  email: string;
+  telephoneNumbers: string;
+  telephone: string;
+  faxNumbers: string;
+  fax: string;
+  mobilNumbers: string;
+  mobil: string;
+  name: string;
+};
 export type AppLink = {
   label: string;
   route: string;
   icon?: string;
   isInToolbar?: boolean;
-  requiresAuth: boolean;
 };
-
-export type InfoText = {
-  title: string;
-  content: string;
-  schedule?: {time:string, age:string}[]; 
-  schedule_title?: string;
-  schedule_days?: string;
-};
-
