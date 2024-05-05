1370e71ccf1a1ba4db3aaecbbb115d26eee0c488
SkazuHD <voda0110@gmail.com>
2024-05-05 13:41:30 +0200

    fix: responsive behaviour of app-shell sidenav

---
+++ apps/web-app/src/app/app-shell/toolbar.component.ts
@@ -0,0 +1,21 @@
+import {Component, input} from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {MatToolbar} from "@angular/material/toolbar";
+import {RouterLink} from "@angular/router";
+import {AppLink} from "../utils/types/types";
+import {MatSidenav} from "@angular/material/sidenav";
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [CommonModule, MatIcon, MatIconButton, MatListItem, MatNavList, MatToolbar, NgOptimizedImage, RouterLink],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.css',
+})
+export class ToolbarComponent {
+  links = input<AppLink[]>();
+  sideNav = input<MatSidenav>();
+}
