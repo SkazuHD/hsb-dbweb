5d57167fe4a459eb84a94219a560c5a35d24f66c
aqfel <131907659+aqfel@users.noreply.github.com>
2024-05-17 13:18:43 +0200

    fix

--- apps/web-app/src/app/app.routes.ts
+++ apps/web-app/src/app/app.routes.ts
@@ -1,8 +1,9 @@
-import {Route} from '@angular/router';
-import {AppComponent} from './app.component';
-import {AppShellComponent} from './app-shell/app-shell';
-import {ArticleComponent} from './components/article/article.component';
-import {InfoPageComponent} from "./components/infopage/info-page.component";
+import { Route } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AppShellComponent } from './app-shell/app-shell';
+import { ArticleComponent } from './components/article/article.component';
+import { InfoPageComponent } from './components/infopage/info-page.component';
+import { LandingPageComponent } from './components/landingPage/landingPage.component';
 
 export const appRoutes: Route[] = [
   {
@@ -16,21 +17,26 @@ export const appRoutes: Route[] = [
           {
             path: 'markdown',
             loadComponent: () =>
-              import("./components/markdown-playground/markdown-playground.component").then(m => m.MarkdownPlaygroundComponent)
+              import(
+                './components/markdown-playground/markdown-playground.component'
+              ).then((m) => m.MarkdownPlaygroundComponent),
           },
-          {path: 'articles', component: ArticleComponent},
-          {path: 'articles/:slugId', component: ArticleComponent},
-          {path: 'profiles/:username', component: AppComponent},
-          {path: 'info', component: InfoPageComponent},
-          {path: 'info/:id', component: InfoPageComponent},
-          {path: 'contact', component: AppComponent},
-          {path: 'impressum', component: AppComponent},
-          {path: 'datenschutz', component: AppComponent},
-          {path: 'sponsor', component: AppComponent},
+          { path: 'articles', component: ArticleComponent },
+          { path: 'articles/:slugId', component: ArticleComponent },
+          { path: 'profiles/:username', component: AppComponent },
+          { path: 'info', component: InfoPageComponent },
+          { path: 'info/:id', component: InfoPageComponent },
+          { path: 'contact', component: AppComponent },
+          { path: 'impressum', component: AppComponent },
+          { path: 'datenschutz', component: AppComponent },
+          { path: 'sponsor', component: AppComponent },
+          { path: 'landingpage', component: LandingPageComponent },
           {
             path: '404',
             loadComponent: () =>
-              import('./components/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)
+              import(
+                './components/page-not-found/page-not-found.component'
+              ).then((m) => m.PageNotFoundComponent),
           },
         ],
       },
--- apps/web-app/src/app/components/newsFeed/newsfeed.component.ts
+++ apps/web-app/src/app/components/newsFeed/newsfeed.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-newsfeed',
   standalone: true,
-  imports: [CommonModule, MatButton],
+  imports: [CommonModule, MatButton, MatCardModule],
   templateUrl: './newsfeed.component.html',
   styleUrl: './newsfeed.component.css',
 })
