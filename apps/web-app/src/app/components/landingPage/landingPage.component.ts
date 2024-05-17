6265b6b00dbb9cdf07cd8eed50fc0b9955827d3c
aqfel <131907659+aqfel@users.noreply.github.com>
2024-05-17 13:18:43 +0200

    fix

--- apps/web-app/src/app/components/landingPage/landingPage.component.ts
+++ apps/web-app/src/app/components/landingPage/landingPage.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButton } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
 import { NewsfeedComponent } from '../newsFeed/newsfeed.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, MatButton, NewsfeedComponent],
+  imports: [CommonModule, MatButton, MatCard, NewsfeedComponent],
   templateUrl: './landingPage.component.html',
   styleUrl: './landingPage.component.css',
 })
