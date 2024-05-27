289b37a9c409837f0ecba76e0347853c1c8d9342
SkazuHD <voda0110@gmail.com>
2024-05-27 12:40:02 +0200

    chore: update deps

--- apps/web-app/src/app/app.config.ts
+++ apps/web-app/src/app/app.config.ts
@@ -1,7 +1,7 @@
-import {ApplicationConfig, EnvironmentProviders, importProvidersFrom, isDevMode,} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, isDevMode,} from '@angular/core';
 import {provideRouter, withComponentInputBinding} from '@angular/router';
 import {appRoutes} from './app.routes';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient} from '@angular/common/http';
 import {provideNativeDateAdapter} from '@angular/material/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AuthInterceptor} from './utils/http-interceptor/auth.interceptor';
@@ -11,29 +11,26 @@ import {getAuth, provideAuth} from '@angular/fire/auth';
 import {provideServiceWorker} from '@angular/service-worker';
 import {AuthService} from "./services/auth.service";
 
-const firebaseProviders: EnvironmentProviders = importProvidersFrom([
-  provideFirebaseApp(() =>
-    initializeApp({
-      apiKey: "AIzaSyAQ2p-heYZkI-vhLOuEoZB3dtLnM-Xcd_Y",
-      authDomain: "hsb-dbweb.firebaseapp.com",
-      databaseURL: "https://hsb-dbweb-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "hsb-dbweb",
-      storageBucket: "hsb-dbweb.appspot.com",
-      messagingSenderId: "1077398966955",
-      appId: "1:1077398966955:web:dca523d5dacb0218cd5dee",
-      measurementId: "G-1TGBM1YNWY"
-    }),
-  ),
-  provideAuth(() => getAuth()),
-]);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withComponentInputBinding()),
     {provide: AuthService},
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(),
     provideNativeDateAdapter(),
-    firebaseProviders,
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: "AIzaSyAQ2p-heYZkI-vhLOuEoZB3dtLnM-Xcd_Y",
+        authDomain: "hsb-dbweb.firebaseapp.com",
+        databaseURL: "https://hsb-dbweb-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "hsb-dbweb",
+        storageBucket: "hsb-dbweb.appspot.com",
+        messagingSenderId: "1077398966955",
+        appId: "1:1077398966955:web:dca523d5dacb0218cd5dee",
+        measurementId: "G-1TGBM1YNWY"
+      }),
+    ),
+    provideAuth(() => getAuth()),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
