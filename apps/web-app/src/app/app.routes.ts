f65784906e04ca23daf59ec8bf9caf540377e041
aqfel <131907659+aqfel@users.noreply.github.com>
2024-06-06 15:14:31 +0200

    feat: landingpage & newsfeed components

--- apps/web-app/src/app/app.routes.ts
+++ apps/web-app/src/app/app.routes.ts
@@ -2,16 +2,19 @@ import {Route} from '@angular/router';
 import {AppComponent} from './app.component';
 import {AppShellComponent} from './app-shell/app-shell';
 import {ArticleComponent} from './components/article/article.component';
-import {InfoPageComponent} from "./components/infopage/info-page.component";
+import {ArticlelistComponent} from './components/articlelist/articlelist.component';
+import {InfoPageComponent} from './components/infopage/info-page.component';
+import {LandingPageComponent} from './components/landingpage/landingPage.component';
+import {adminGuard} from './utils/guards/admin.guard';
 import {ContactComponent} from './components/contacts/contact.component';
-import {adminGuard} from "./utils/guards/admin.guard";
 
 export const appRoutes: Route[] = [
   {
     path: '',
     component: AppShellComponent,
     children: [
-      {path: 'articles', component: ArticleComponent},
+      {path: '', component: LandingPageComponent},
+      {path: 'articles', component: ArticlelistComponent},
       {path: 'articles/:slugId', component: ArticleComponent},
       {path: 'profiles/:username', component: AppComponent},
       {path: 'info', component: InfoPageComponent},
@@ -20,19 +23,27 @@ export const appRoutes: Route[] = [
       {path: 'impressum', component: AppComponent},
       {path: 'datenschutz', component: AppComponent},
       {path: 'sponsor', component: AppComponent},
-      {path: 'demo', loadComponent: () => import('./demo/demo.component').then(m => m.DemoComponent)},
+
+      {
+        path: 'demo',
+        loadComponent: () =>
+          import('./demo/demo.component').then((m) => m.DemoComponent),
+      },
       {
         path: '404',
         loadComponent: () =>
-          import('./components/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)
+          import('./components/page-not-found/page-not-found.component').then(
+            (m) => m.PageNotFoundComponent,
+          ),
       },
     ],
   },
   {
     path: 'admin',
-    loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent),
+    loadComponent: () =>
+      import('./admin/admin.component').then((m) => m.AdminComponent),
     canActivate: [adminGuard],
-    children: []
+    children: [],
   },
   {
     path: '**',
---
+++ apps/web-app/src/app/components/landingpage/landingPage.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject, model, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
+import { NewsfeedComponent } from '../newsfeed/newsfeed.component';
+import { ApiService } from '../../services/api.service';
+import { Article, Image } from '@hsb-dbweb/shared';
+import { RouterModule } from '@angular/router';
+import { url } from 'inspector';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [CommonModule, MatButton, MatCard, NewsfeedComponent, RouterModule],
+  templateUrl: './landingPage.component.html',
+  styleUrl: './landingPage.component.css',
+})
+export class LandingPageComponent implements OnInit {
+  apiService: ApiService = inject(ApiService);
+  articles = model<Article[]>();
+  landingpageimage: Image = {
+    url: 'https://tv.orf.at/program/orf1/kungfupanda-aufmach100~_v-epg__large__16__9_-5412e775eb65789c908def5fa9fdf24a7b895a8f.jpg',
+    alt: 'Decription of the image',
+  };
+  landingpagetext = 'Hier Abouttext einfügen';
+
+  ngOnInit() {
+    this.apiService.getArticles().subscribe((articles) => {
+      this.articles.set(articles);
+    });
+  }
+}
