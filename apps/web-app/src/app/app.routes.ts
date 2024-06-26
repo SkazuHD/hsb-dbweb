e9250769b18ec0fa6d213b1d2a9b94e9b6b3a832
SkazuHD <voda0110@gmail.com>
2024-06-26 12:31:39 +0200

    feat: Eventpage

--- apps/web-app/src/app/app.routes.ts
+++ apps/web-app/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import {ArticleComponent} from './components/article/article.component';
 import {InfoPageComponent} from "./components/infopage/info-page.component";
 import {ContactComponent} from './components/contacts/contact.component';
 import {adminGuard} from "./utils/guards/admin.guard";
+import {EventpageComponent} from "./components/event-page/eventpage.component";
 import { LandingPageComponent } from './components/landingpage/landingPage.component';
 import { ArticlelistComponent } from './components/articlelist/articlelist.component';
 import {GalleryComponent} from './components/gallery/gallery.component';
@@ -22,6 +23,7 @@ export const appRoutes: Route[] = [
       {path: 'info', component: InfoPageComponent},
       {path: 'info/:id', component: InfoPageComponent},
       {path: 'contact', component: ContactComponent},
+      {path: 'events', component: EventpageComponent},
       {path: 'impressum', component: AppComponent},
       {path: 'datenschutz', component: AppComponent},
       {path: 'sponsor', component: AppComponent},
