2499ef0613680374f04babddee446987b0810bf4
SkazuHD <voda0110@gmail.com>
2024-06-29 13:27:59 +0200

    feat: improved articlelist component

--- apps/web-app/src/app/components/articlelist/articlelist.component.ts
+++ apps/web-app/src/app/components/articlelist/articlelist.component.ts
@@ -1,11 +1,11 @@
-import { Component, model, inject, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatButton } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { RouterModule } from '@angular/router';
-import { Article } from '@hsb-dbweb/shared';
-import { ApiService } from '../../services/api.service';
-import { NewsfeedComponent } from '../newsfeed/newsfeed.component';
+import {Component, computed, inject, input, model, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatButton} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {RouterModule} from '@angular/router';
+import {Article} from '@hsb-dbweb/shared';
+import {ApiService} from '../../services/api.service';
+import {NewsfeedComponent} from '../newsfeed/newsfeed.component';
 
 @Component({
   selector: 'app-articlelist',
@@ -22,9 +22,16 @@ import { NewsfeedComponent } from '../newsfeed/newsfeed.component';
 })
 export class ArticlelistComponent implements OnInit {
   apiService: ApiService = inject(ApiService);
-  articles = model<Article[]>();
+  articles = model<Article[]>([]);
+  title = input<string>('Neuigkeiten');
+  limit = input<number>(3);
+  articleList = computed(() => {
+    if (this.limit() === 0) return this.articles();
+    return this.articles()?.slice(0, this.limit())
+  });
 
   ngOnInit() {
+    if (this.articles()?.length > 0) return;
     this.apiService.getArticles().subscribe((articles) => {
       this.articles.set(articles);
     });
